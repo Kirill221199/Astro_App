8eb702e968751617ffe33d075b277e9566b2345e
Кирилл <agarin221199@mail.ru>
2022-05-25 19:04:07 +0500

    Added Bottom Sheet

--- app/src/main/java/ru/kirill/astro_app/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/kirill/astro_app/view/picture/PictureOfTheDayFragment.kt
@@ -1,6 +1,7 @@
 package ru.kirill.astro_app.view.picture
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.kirill.astro_app.databinding.FragmentPictureOfTheDayBinding
 import ru.kirill.astro_app.viewmodel.PictureOfTheDayAppState
 import ru.kirill.astro_app.viewmodel.PictureOfTheDayViewModel
@@ -32,12 +34,15 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRequest()
+        initBehaviorBottomSheet()
+    }
 
+    private fun initRequest() {
         viewModel.getLiveData().observe(viewLifecycleOwner, Observer {
             renderData(it)
         })
         viewModel.sendRequest()
-
     }
 
     private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState) {
@@ -47,8 +52,34 @@ class PictureOfTheDayFragment : Fragment() {
             is PictureOfTheDayAppState.Success -> {
                 binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
                 // Убрать задержку загрузки картинки placeHolder
+                binding.lifeHack.title.text = pictureOfTheDayAppState.pictureOfTheDayResponseData.title
+                binding.lifeHack.explanation.text = pictureOfTheDayAppState.pictureOfTheDayResponseData.explanation
+
             }
         }
+    }
+
+    private fun initBehaviorBottomSheet(){
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.lifeHack.bottomSheetContainer)
+        //bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> {}
+                    BottomSheetBehavior.STATE_COLLAPSED -> {}
+                    BottomSheetBehavior.STATE_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HIDDEN -> {}
+                    BottomSheetBehavior.STATE_SETTLING -> {}
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("@@@", "$slideOffset")
+            }
+
+        })
     }
 
 
