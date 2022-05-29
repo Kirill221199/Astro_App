373f96fbc0322cfaab6c8e3ef648f735f15001aa
Кирилл <agarin221199@mail.ru>
2022-05-29 18:13:17 +0500

    Implemented a change of the application theme

--- app/src/main/java/ru/kirill/astro_app/view/MainActivity.kt
+++ app/src/main/java/ru/kirill/astro_app/view/MainActivity.kt
@@ -5,14 +5,44 @@ import android.os.Bundle
 import ru.kirill.astro_app.R
 import ru.kirill.astro_app.view.picture.PictureOfTheDayFragment
 
+const val ThemeEarth = 0
+const val ThemeMoon = 1
+const val ThemeMars = 2
+
 class MainActivity : AppCompatActivity() {
+
+    private val KEY_SP = "sp"
+    private val KEY_THEME = "current_theme"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(getRealStyle(getCurrentTheme()))
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        }
+    }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_THEME, currentTheme)
+        editor.apply()
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_THEME, -1)
+    }
+
+    private fun getRealStyle(currentTheme: Int): Int {
+        return when (currentTheme) {
+            ThemeEarth -> R.style.Earth
+            ThemeMoon -> R.style.Moon
+            ThemeMars -> R.style.Mars
+            else -> 0
         }
     }
 }
--- app/src/main/java/ru/kirill/astro_app/view/SettingsFragment.kt
+++ app/src/main/java/ru/kirill/astro_app/view/SettingsFragment.kt
@@ -1,11 +1,11 @@
 package ru.kirill.astro_app.view
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import coil.load
 import coil.transform.CircleCropTransformation
@@ -15,6 +15,14 @@ import ru.kirill.astro_app.databinding.FragmentSettingsBinding
 
 
 class SettingsFragment : Fragment() {
+    private val KEY_SP_SETTINGS = "sp_settings"
+    private val KEY_THEME_SETTINGS = "current_theme_settings"
+
+    private lateinit var parentActivity: MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = (context as MainActivity)
+    }
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
@@ -23,38 +31,68 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val tab = binding.tabLayoutSettings.getTabAt(getPositionTheme())
+        tab?.select()
+        binding.imageView.load(R.drawable.earth) {
+            transformations(CircleCropTransformation())
+        }
         initTab()
+        setChooseTheme()
     }
 
     private fun initTab(){
         binding.tabLayoutSettings.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab?.position) {
-                    0 -> {binding.imageView.load(R.drawable.earth){
-                        transformations(CircleCropTransformation())}}
-                    1 -> {binding.imageView.load(R.drawable.moon){
-                        transformations(CircleCropTransformation())}}
-                    2 -> {binding.imageView.load(R.drawable.mars){
-                        transformations(CircleCropTransformation())
-                    }}
+                    0 -> { binding.imageView.load(R.drawable.earth) {
+                        transformations(CircleCropTransformation()) }
+                    }
+                    1 -> { binding.imageView.load(R.drawable.moon) {
+                        transformations(CircleCropTransformation()) }
+                    }
+                    2 -> { binding.imageView.load(R.drawable.mars) {
+                        transformations(CircleCropTransformation()) }
+                    }
                 }
+                setPositionTheme(tab!!.position)
             }
+
             override fun onTabUnselected(tab: TabLayout.Tab?) {
                 //что-то реализовать
             }
+
             override fun onTabReselected(tab: TabLayout.Tab?) {
                 //что-то реализовать
             }
         })
     }
 
+    private fun setChooseTheme() {
+        binding.buttonSettingsChoose.setOnClickListener {
+            parentActivity.setCurrentTheme(getPositionTheme())
+            parentActivity.recreate()
+        }
+    }
+
+    private fun setPositionTheme(positionTheme: Int) {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(KEY_SP_SETTINGS, AppCompatActivity.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_THEME_SETTINGS, positionTheme)
+        editor.apply()
+    }
+
+    private fun getPositionTheme(): Int {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(KEY_SP_SETTINGS, AppCompatActivity.MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_THEME_SETTINGS, -1)
+    }
 
     companion object {
         @JvmStatic
@@ -69,4 +107,5 @@ class SettingsFragment : Fragment() {
         _binding = null
         super.onDestroy()
     }
+
 }
