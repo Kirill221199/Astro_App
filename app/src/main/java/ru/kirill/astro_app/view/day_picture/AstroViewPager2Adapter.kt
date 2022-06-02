7d008c1bfdafda1345b0667dd48a62769c3cd696
Кирилл <agarin221199@mail.ru>
2022-06-02 20:29:40 +0500

    Added ViewPager2+TabLayout in PictureOfTheDayFragment.kt

--- app/src/main/java/ru/kirill/astro_app/view/day_picture/AstroViewPager2Adapter.kt
+++ app/src/main/java/ru/kirill/astro_app/view/day_picture/AstroViewPager2Adapter.kt
@@ -1,23 +1,19 @@
 package ru.kirill.astro_app.view.day_picture
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class AstroViewPager2Adapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+class AstroViewPager2Adapter(fa: Fragment) : FragmentStateAdapter(fa) {
 
-    private val today = PictureTodayFragment
-    private val yesterday = PictureYesterdayFragment
-    private val tdby = PictureTdbyFragment
+    val today:Fragment = PictureTodayFragment.newInstance()
+    val yesterday:Fragment = PictureYesterdayFragment.newInstance()
+    val tdby:Fragment = PictureTdbyFragment.newInstance()
     private val fragments = arrayOf(today,yesterday,tdby)
 
     override fun getItemCount(): Int = fragments.size
 
     override fun createFragment(position: Int): Fragment {
-        return when(position){
-            0-> today as Fragment
-            1-> yesterday as Fragment
-            else -> tdby as Fragment
-        }
+        return fragments[position]
     }
+
 }
--- app/src/main/java/ru/kirill/astro_app/view/day_picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/kirill/astro_app/view/day_picture/PictureOfTheDayFragment.kt
@@ -13,6 +13,7 @@ import coil.load
 import coil.transform.CircleCropTransformation
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import ru.kirill.astro_app.R
 import ru.kirill.astro_app.databinding.FragmentPictureOfTheDayBinding
 import ru.kirill.astro_app.viewmodel.PictureOfTheDayAppState
@@ -25,23 +26,6 @@ class PictureOfTheDayFragment : Fragment() {
 
     private var _binding: FragmentPictureOfTheDayBinding? = null
     private val binding get() = _binding!!
-    lateinit var today:String
-
-    private val customFormatter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        DateTimeFormatter.ofPattern("uuuu-MM-d")
-    } else {
-        TODO("VERSION.SDK_INT < O")
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    val yesterday = LocalDateTime.now().minusDays(2).format(customFormatter)!!
-    @RequiresApi(Build.VERSION_CODES.O)
-    val tdby = LocalDateTime.now().minusDays(3).format(customFormatter)!!
-
-
-    private val viewModel: PictureOfTheDayViewModel by lazy {
-        ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,112 +35,23 @@ class PictureOfTheDayFragment : Fragment() {
         return binding.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setMargins(binding.tabLayoutMain, 0, binding.tvPicture.getWidthHeight().second, 0, 0)
-        initRequest("")
-        initBehaviorBottomSheet()
+        binding.viewPagerPictureOfTheDay.adapter = AstroViewPager2Adapter(this)
         setTab()
     }
 
-    private fun setMargins(view: View, left: Int, top: Int, right: Int, bottom: Int) {
-        if (view.layoutParams is MarginLayoutParams) {
-            val p = view.layoutParams as MarginLayoutParams
-            p.setMargins(left, top, right, bottom)
-            view.requestLayout()
-        }
-    }
 
-    // Костыль чтобы узнать высоту элемента
-    fun View.getWidthHeight():Pair<Int,Int>{
-        measure(
-            // horizontal space requirements as imposed by the parent
-            0, // widthMeasureSpec
-
-            // vertical space requirements as imposed by the parent
-            0 // heightMeasureSpec
-        )
-        // the raw measured width of this view
-        val width = measuredWidth
-        // the raw measured height of this view
-        val height = measuredHeight
-
-        // return view's width and height in pixels
-        return Pair(width,height)
-    }
-
-    private fun initRequest(date: String) {
-        viewModel.getLiveData().observe(viewLifecycleOwner, Observer {
-            renderData(it)
-        })
-        viewModel.sendRequest(date)
-    }
-
-
-    private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState) {
-        when (pictureOfTheDayAppState) {
-            is PictureOfTheDayAppState.Error -> {}
-            is PictureOfTheDayAppState.Loading -> {}
-            is PictureOfTheDayAppState.Success -> {
-                binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url){
-                    placeholder(R.drawable.space)
-                    error(R.drawable.ic_baseline_error_outline_24)
-                    transformations(CircleCropTransformation())
-                }
-                binding.lifeHack.title.text = pictureOfTheDayAppState.pictureOfTheDayResponseData.title
-                binding.lifeHack.explanation.text = pictureOfTheDayAppState.pictureOfTheDayResponseData.explanation
-                today = pictureOfTheDayAppState.pictureOfTheDayResponseData.date
-                Log.d("@@@", "$today")
-            }
-        }
-    }
-
-    private fun initBehaviorBottomSheet(){
-        val bottomSheetBehavior = BottomSheetBehavior.from(binding.lifeHack.bottomSheetContainer)
-        bottomSheetBehavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
-            val color = 123456
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                when (newState) {
-                    BottomSheetBehavior.STATE_DRAGGING -> {}
-                    BottomSheetBehavior.STATE_COLLAPSED -> {}
-                    BottomSheetBehavior.STATE_EXPANDED -> {
-                        binding.tabLayoutMain.visibility = View.GONE
-                        binding.tvPicture.visibility = View.GONE
-                    }
-                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {}
-                    BottomSheetBehavior.STATE_HIDDEN -> {}
-                    BottomSheetBehavior.STATE_SETTLING -> {
-                        binding.tabLayoutMain.visibility = View.VISIBLE
-                        binding.tvPicture.visibility = View.VISIBLE
-                    }
+    private fun setTab(){
+        TabLayoutMediator(binding.tabLayoutMain,binding.viewPagerPictureOfTheDay,object :TabLayoutMediator.TabConfigurationStrategy{
+            override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                tab.text = when(position){
+                    0-> "today"
+                    1-> "yesterday"
+                    else -> "tdby"
                 }
             }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                Log.d("@@@", "$slideOffset")
-            }
-
-        })
-    }
-
-    private fun setTab() {
-        binding.tabLayoutMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab?.position) {
-                    0 -> {initRequest("")}
-                    1 -> {initRequest(yesterday)}
-                    2 -> {initRequest(tdby)}
-                }
-            }
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                //что-то реализовать
-            }
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                //что-то реализовать
-            }
-        })
+        } ).attach()
     }
 
     companion object {
