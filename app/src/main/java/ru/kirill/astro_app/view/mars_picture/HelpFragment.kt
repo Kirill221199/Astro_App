c9c951478b6611e70744aabf3a6fd9155b9045ca
Кирилл <agarin221199@mail.ru>
2022-06-03 23:10:43 +0500

    Finished help (fragment to mars picture)

--- app/src/main/java/ru/kirill/astro_app/view/mars_picture/HelpFragment.kt
+++ app/src/main/java/ru/kirill/astro_app/view/mars_picture/HelpFragment.kt
@@ -1,12 +1,20 @@
 package ru.kirill.astro_app.view.mars_picture
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.kirill.astro_app.R
+import ru.kirill.astro_app.databinding.FragmentHelpBinding
+import ru.kirill.astro_app.databinding.FragmentWikiSearchBinding
+
 class HelpFragment : Fragment() {
+
+    private var _binding: FragmentHelpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +27,22 @@ class HelpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false)
+        _binding = FragmentHelpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tapLink()
+    }
+
+    private fun tapLink(){
+        binding.link.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data =
+                    Uri.parse("https://api.nasa.gov/index.html#browseAPI")
+            })
+        }
     }
 
     companion object {
@@ -29,5 +52,10 @@ class HelpFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
